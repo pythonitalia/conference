972dd99c61641c65eed967552bc296577a0c65fb
David Mugnai <dvd@develer.com>
2015-04-13 15:46:20 +0200

    interfaccia generazione badge -> conf
    più funzioni esposte
    correzione bug lettere sotto la baseline

--- conference/utils/ticket_badge.py
+++ conference/utils/ticket_badge.py
@@ -7,7 +7,7 @@ import os
 import os.path
 import re
 import sys
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont, ImageMath
 from itertools import izip_longest
 
 parser = optparse.OptionParser(usage='%(prog)s [options] output_dir')
@@ -125,12 +125,77 @@ def wrap_text(font, text, width):
 def draw_info(image, max_width, text, pos, font, color, line_offset=8):
     d = ImageDraw.Draw(image)
 
+    lowline_check = 'gjqpy'
+
     cx = pos[0]
     cy = pos[1] - font.getsize(text)[1]
+    if set(lowline_check) & set(text):
+        diff = font.getsize('g')[1] - font.getsize('o')[1]
+        cy += diff
+        line_offset += diff
     lines = wrap_text(font, text, max_width)
     for l in lines:
         d.text((cx, cy), l, font = font, fill = color)
         cy += font.getsize(l)[1] + line_offset
+
+    return len(lines), cy
+
+
+def open_font(file_path, points, _cache={}):
+    """
+    Open a truetype font and set the size to the specified in points.
+    """
+    try:
+        return _cache[(file_path, points)]
+    except KeyError:
+        f = _cache[(file_path, points)] = ImageFont.truetype(file_path, points * DPI/72)
+        return f
+
+
+def ticket_group(ticket):
+    return ticket['_ticket_group']
+
+
+# http://stackoverflow.com/questions/765736/using-pil-to-make-all-white-pixels-transparent#answer-4531395
+def distance2(a, b):
+    return (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]) + (a[2] - b[2]) * (a[2] - b[2])
+
+
+def makeColorTransparent(image, color, thresh2=0):
+    image = image.convert("RGBA")
+    red, green, blue, alpha = image.split()
+    image.putalpha(ImageMath.eval("""convert(((((t - d(c, (r, g, b))) >> 31) + 1) ^ 1) * a, 'L')""",
+                   t=thresh2, d=distance2, c=color, r=red, g=green, b=blue, a=alpha))
+    return image
+
+
+def split_name(name):
+    """
+    Split a name in two pieces: first_name, last_name.
+    """
+    parts = name.split(' ')
+    if len(parts) == 4 and parts[2].lower() not in ('de', 'van'):
+        first_name = ' '.join(parts[:2])
+        last_name = ' '.join(parts[2:])
+    else:
+        first_name = parts[0]
+        last_name = ' '.join(parts[1:])
+    return first_name.strip(), last_name.strip()
+
+
+def open_auxiliary_image(path, mm, transparent_pixel=(0, 0), threshold=150):
+    img = Image.open(path)
+    if img.mode != 'RGBA':
+        if img.mode == 'LA':
+            img = img.convert('RGBA')
+        else:
+            if img.mode != 'RGB':
+                img = img.convert('RGB')
+            if transparent_pixel:
+                img = makeColorTransparent(img, img.getpixel(transparent_pixel), thresh2=threshold)
+    size = map(int, (mm * MM2INCH * DPI, mm * MM2INCH * DPI))
+    return img.resize(size, Image.ANTIALIAS)
+
 
 def assemble_page(images, align='left', mirror_x=False):
     page = Image.new('RGBA', PAGE_SIZE, (255, 255, 255, 255))
@@ -191,6 +256,10 @@ badge_x_mirror = opts.mirror_x
 for group_type, data in sorted(groups.items()):
     image = data['image']
     attendees = data['attendees']
+    if 'mirror_x' in data:
+        group_x_mirror = data['mirror_x']
+    else:
+        group_x_mirror = badge_x_mirror
     pages = len(attendees) / opts.per_page
     if len(attendees) % opts.per_page:
         pages += 1
@@ -198,15 +267,22 @@ for group_type, data in sorted(groups.items()):
     utils = {
         'wrap_text': wrap_text,
         'draw_info': draw_info,
+        'open_font': open_font,
+        'make_color_transparent': makeColorTransparent,
+        'split_name': split_name,
+        'ticket_group': ticket_group,
+        'open_auxiliary_image': open_auxiliary_image,
     }
     count = 1
     for block in grouper(opts.per_page, attendees):
         if block:
             images = []
             for a in block:
+                if a:
+                    a['_ticket_group'] = group_type
                 badge = render_badge(image, a, utils=utils, resize_factor=opts.resize)
                 images.append(badge)
-            page = assemble_page(images, badge_align, badge_x_mirror)
+            page = assemble_page(images, badge_align, group_x_mirror)
 
             name = '[%s] pag %s-%s.tif' % (group_type, str(count).zfill(2), str(pages).zfill(2))
             print >>sys.stderr, name
